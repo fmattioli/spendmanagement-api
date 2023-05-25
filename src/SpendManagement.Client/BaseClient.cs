d1a40ed83f1e566b1936db424ec3f17a0ec6c37a
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-05-26 00:29:59 +0100

    Starting the development of the new endpoint to update a receipt

---
+++ src/SpendManagement.Client/BaseClient.cs
@@ -0,0 +1,27 @@
+﻿using SpendManagement.Client.Configuration;
+using System.Net.Http.Json;
+
+namespace SpendManagement.Client
+{
+    public class BaseClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly Uri baseUri;
+        public BaseClient(HttpClient httpClient, IApiConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            this.baseUri = new Uri(
+                new Uri(configuration.Endpoint),
+                $"{configuration.Version}");
+        }
+
+        protected async Task<TResponse?> GetAsync<TResponse>(
+            IDictionary<string, object> queryParams)
+           where TResponse : class
+        {
+            var response = await this._httpClient.GetFromJsonAsync<TResponse>($"{baseUri.AbsoluteUri}/GetReceipt/{queryParams}");
+
+            return response;
+        }
+    }
+}
---
+++ src/SpendManagement.Infra.IOC/Conf/SpendManagementReadModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SpendManagement.Infra.CrossCutting.Conf
+{
+    public class SpendManagementReadModel
+    {
+        public string Url { get; set; } = null!;
+        public short MaxRetry { get; set; }
+    }
+}
