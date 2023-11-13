09dc845dcb466df0095dd59bd59e5aa61d6e2f36
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2023-11-13 11:20:00 -0300

    FEAT: Starting the development new integration tests

---
+++ src/SpendManagement.Integration.Tests/Configuration/MongoSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SpendManagement.Integration.Tests.Configuration
+{
+    public record MongoSettings
+    {
+        public string Database { get; set; } = null!;
+        public string ConnectionString { get; set; } = null!;
+    }
+}
